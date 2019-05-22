68d37b6c966e796c54a6eb917f25b9266ff2b0b1
carinadidomenico <carinadidomenico@gmail.com>
2019-05-22 17:49:27 -0300

    Para quem e Como funciona

--- js/script.js
+++ js/script.js
@@ -2,5 +2,12 @@ let slide = document.querySelectorAll('.slide');
 let img = document.querySelectorAll('.function__content--img');
 
 function slideText () {
-    
+    slide.innerHTML = "Tire suas dúvidas";
+    slide.style.backgroundColor = "#373849";
 }
+
+img.addEventListener ('mouseover', function slideText(){
+    console.log ("oi inhai");
+    slide.innerHTML = "Tire suas dúvidas";
+    slide.style.backgroundColor = "#373849";
+});
