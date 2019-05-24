fff30d76a58bdc4f0d600f1e4792b10d619a7b1f
carinadidomenico <carinadidomenico@gmail.com>
2019-05-24 00:11:58 -0300

    Ajuste responsividade

--- js/script.js
+++ js/script.js
@@ -4,49 +4,39 @@ let btnRevenue = document.querySelector(".btn--revenue");
 
 let slideBook = document.querySelector (".slide__book")
 let slideTry = document.querySelector (".slide__try");
-let slideRevenues = document.querySelector (".slide__revenue")
-
-
-
+let slideRevenues = document.querySelector (".slide__revenue");
 
 
 btnBook.addEventListener ('click', function(){
+  
+  if (slideBook.style.display === "none") {
+    slideBook.style.display = "block";
+    btnBook.innerHTML = "-";
 
-    slideBook.classList.toggle("active");
-    // slideBook.animate ([
-    //     {transform: "translateY(0px)"},
-    //     {transform: "translateY(300px)"}
-
-    // ], 
-    // {duration: 1000, width: 60%})
-
-    
-    if (slideBook.style.display === "none") {
-        slideBook.style.display = "block";
-        btnBook.innerHTML = "-"
-      } else {
-        slideBook.style.display = "none"
-        btnBook.innerHTML = "+"
-      }
-
+  } else {
+    slideBook.style.display = "none";
+    btnBook.innerHTML = "+";
+  };
 });
 
 btnTry.addEventListener ('click', function(){
-    
-    if (slideTry.style.display === "none") {
-        slideTry.style.display = "block";
-      } else {
-        slideTry.style.display = "none"
-      }
-
+  
+  if (slideTry.style.display === "none") {
+    slideTry.style.display = "block";
+    btnTry.innerHTML = "-";
+  } else {
+    slideTry.style.display = "none";
+    btnTry.innerHTML = "+";
+  };
 });
 
 btnRevenue.addEventListener ('click', function(){
-    
-    if (slideRevenues.style.display === "none") {
-        slideRevenues.style.display = "block";
-      } else {
-        slideRevenues.style.display = "none"
-      }
-    
+  
+  if (slideRevenues.style.display === "none") {
+    slideRevenues.style.display = "block";
+    btnRevenue.innerHTML = "-";
+  } else {
+    slideRevenues.style.display = "none";
+    btnRevenue.innerHTML = "+";
+  };  
 });
